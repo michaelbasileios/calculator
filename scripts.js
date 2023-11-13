141e3086fa78bf52b13f96cf21f6e51a4cb34ba3
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-11-13 15:37:00 -0600

    Add operate function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 //BASIC OPS CODE
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
 let num1;
 let num2;
 let operator;
 
-const operate = (num1, num2) => 
+function operate(x, y, operation) {
+    const operators = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b,
+      '%': (a, b) => a % b,
+      '**': (a, b) => a ** b
+    }
+  
+    return operation in operators ? operators[operation](x, y) : NaN
+  }
