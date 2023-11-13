c1cc092cc50feb7e4d2dc30944cb8b6eac118c1a
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-11-13 10:35:22 -0600

    Add equals and clear buttons

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//BASIC OPS CODE
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let num1;
+let num2;
+let operator;
+
+const operate = (num1, num2) => 
