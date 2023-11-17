2106cd007f17cca89d6af059a62a7db226bc1add
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-11-17 17:16:02 -0600

    Introduce selectors for equals and operators

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 //BASIC OPS CODE
 let num1;
 let num2;
-let operator;
+let operator = "";
 
 function operate(x, y, operation) {
     const operators = {
@@ -16,22 +16,25 @@ function operate(x, y, operation) {
     return operation in operators ? operators[operation](x, y) : NaN
   }
 
-  const numBtn = document.querySelectorAll('.number-btn');
-  const inputField = document.querySelector('#user-input');
-  const outputField = document.querySelector('#output-field');
+const numBtn = document.querySelectorAll('.number-btn');
+const operatorBtn = document.querySelectorAll('.op-btn');
+const equalsBtn = document.querySelector('#equals-btn')
+const inputField = document.querySelector('#user-input');
+const outputField = document.querySelector('#output-field');
   
-  //CLEAR BTN CODE
-  document.querySelector('#clear-btn').addEventListener('click', () => {
-    outputField.innerText = '';
-  })
+//CLEAR BTN CODE
+document.querySelector('#clear-btn').addEventListener('click', () => {
+  inputField.value = '';
+  outputField.innerText = '';
+})
 
+//CODE TO DISPLAY USER INPUT NUMBERS
+numBtn.forEach(btn => {
+  btn.addEventListener('click', numInput);
+})
 
-  numBtn.forEach(btn => {
-    btn.addEventListener('click', numInput);
-  })
-
-  function numInput(e) {
-    const num1 = e.target.innerText;
-    inputField.value += num1;
-  }
+function numInput(e) {
+  const num1 = e.target.innerText;
+  inputField.value += num1;
+}
 
